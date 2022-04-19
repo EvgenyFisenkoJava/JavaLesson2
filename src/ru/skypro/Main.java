838257d2089c5b36f26a1b2dc30a90904994eb4d
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2022-04-19 22:52:45 +1000

    task1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        byte a = 123;
+        short b = 1234;
+        int c = 123456;
+        long d = 123_456_789L;
+        double e = 1.23;
+        float f = 2.34f;
+        char g = '!';
+        boolean h = true;
+
     }
 }
+
